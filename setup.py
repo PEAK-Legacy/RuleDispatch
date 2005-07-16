c3a0b67f0034fdd0eaa72cb79c8dd6f65a70d662
PJ Eby <pje@telecommunity.com>
2005-07-16 16:26:10 +0000

    Add RuleDispatch package, split out from PyProtocols
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/RuleDispatch@2081 571e12c6-e1fa-0310-aee7-ff1267fa46bd

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""Distutils setup file
+
+RuleDispatch is Copyright 2004, 2005 by Phillip J. Eby; all rights reserved.
+This software may be used under the same terms as Python or Zope, with NO
+WARRANTIES OF ANY KIND WHATSOEVER.
+"""
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, Feature, Extension, find_packages
+
+speedups = Feature(
+    "optional C speed-enhancement modules",
+    standard = True,
+    ext_modules = [
+        Extension("dispatch._speedups", ["src/dispatch/_speedups.pyx"]),
+    ]
+)
+
+setup(
+    name="RuleDispatch",
+    version="0.5a0",
+    description="Rule-based Dispatching and Generic Functions",
+    author="Phillip J. Eby",
+    author_email="peak@eby-sarna.com",
+    license="PSF or ZPL",
+    install_requires = ['PyProtocols>=1.0a0dev'],
+    #url="http://peak.telecommunity.com/PyProtocols.html",
+    zip_safe    = True,
+    test_suite  = 'dispatch.tests.test_suite',
+    package_dir = {'':'src'},
+    package_data = {'': ['*.txt']},
+    packages    = find_packages('src'),
+    features    = {'speedups': speedups}
+)
+
